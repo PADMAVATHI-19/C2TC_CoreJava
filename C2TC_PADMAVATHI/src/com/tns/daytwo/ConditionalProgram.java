176562c6dae65e79021c8cb4c3b4ea2c6caf6a9f
ELCOT <ELCOT@Lenovo>
2024-09-04 15:38:59 +0530

    This is my third commit in second program

---
+++ C2TC_PADMAVATHI/src/com/tns/daytwo/ConditionalProgram.java
@@ -0,0 +1,21 @@
+package com.tns.daytwo;
+
+public class ConditionalProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 5; 
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+	}
+
+	}
+
+
